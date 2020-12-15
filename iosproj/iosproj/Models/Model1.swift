18da306e69408d7cce241f626305158898f25aa1
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2020-12-15 22:48:46 +0100

    Feat: add archi

---
+++ iosproj/iosproj/Models/Model1.swift
@@ -0,0 +1,8 @@
+//
+//  Model1.swift
+//  iosproj
+//
+//  Created by Jacky LIM on 15/12/2020.
+//
+
+import Foundation
---
+++ iosproj/iosproj/Views/LoadingUiView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingUiView.swift
+//  iosproj
+//
+//  Created by Jacky LIM on 15/12/2020.
+//
+
+import UIKit
+
+class LoadingUiView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
