9220bc0e32c663e02f8c790973fb8c0e2738459f
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2020-12-17 12:57:11 +0100

    feat: add commentary

--- iosproj/iosproj/Controllers/DiscoverPokemonViewController.swift
+++ iosproj/iosproj/Controllers/DiscoverPokemonViewController.swift
@@ -11,8 +11,15 @@ class DiscoverPokemonViewController: UIViewController {
     
     let pokemonService = PokemonService.shared
     
-    var currentDisplayedPokemon: Any = ""
+    // Créer une variable gesture
+    // Autoriser la gesture sur la vue (view)
+    // Definir la gesture (swip/tap/pinch)
+    // Creer une fonction d'action (@objec func) pour charger une nouveau pokemon dependant de la gesture (random integer entre 1 et 155)
+    // Créer une variable pokemons de type array de pokemon
+    // récuperer les datas d'un pokémon grace au pokemon service
+    // afficher les datas du pokemon sur l'ecran
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
