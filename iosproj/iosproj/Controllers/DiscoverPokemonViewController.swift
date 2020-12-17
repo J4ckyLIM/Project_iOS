aac2d8f7fd917c5fd99da6da8331f55d8b55a11e
Tiffany <n.tiffany.th@gmail.com>
2020-12-17 19:22:01 +0100

    Developpement de lecran Decouverte

--- iosproj/iosproj/Controllers/DiscoverPokemonViewController.swift
+++ iosproj/iosproj/Controllers/DiscoverPokemonViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class DiscoverPokemonViewController: UIViewController {
     
-    let pokemonService = PokemonService.shared
+   
+    @IBOutlet weak var StackViewPokemon: UIStackView!
+    @IBAction func SwipeAction(_ sender: Any) {
+        print("jai swipe")
+    }
+    @IBOutlet weak var imgPok: UIImageView!
+    @IBOutlet weak var typePokLabel: UILabel!
+    @IBOutlet weak var namePokLabel: UILabel!
+    // let pokemonService = PokemonService.shared
+    var pokemonArray = [Pokemon]()
+   
+    
     
     // Créer une variable gesture
     // Autoriser la gesture sur la vue (view)
@@ -19,13 +30,66 @@ class DiscoverPokemonViewController: UIViewController {
     // récuperer les datas d'un pokémon grace au pokemon service
     // afficher les datas du pokemon sur l'ecran
 
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
+        StackViewPokemon.isUserInteractionEnabled = true
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture(sendr:)))
+        swipeRight.direction = .right
+        StackViewPokemon.addGestureRecognizer(swipeRight)
+            
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture(sendr:)))
+        swipeLeft.direction = .left
+        StackViewPokemon.addGestureRecognizer(swipeLeft)
+      //  let randomNumber=Int.random(in: 0...150)
         // Do any additional setup after loading the view.
+      PokemonService.shared.fetchPokemon { (pokemonArray) in
+        print("okok")
+        DispatchQueue.main.sync{
+            self.namePokLabel.text = pokemonArray[0].name
+            self.typePokLabel.text = pokemonArray[0].type
+            self.imgPok.image = pokemonArray[0].image
+            print(pokemonArray[0].name)
+                        }
+     
+        }
+        
+ 
     }
     
+    @objc func swipeGesture(sendr: UISwipeGestureRecognizer?) {
+      //  let randomNumber=Int.random(in: 0...150)
+        if let swipeGesture = sendr {
+            switch swipeGesture.direction {
+            case .right:
+                print("droite")
+                PokemonService.shared.fetchPokemon { (pokemonArray) in
+                DispatchQueue.main.sync{
+                    self.namePokLabel.text = pokemonArray[0].name
+                    self.typePokLabel.text = pokemonArray[0].type
+                    self.imgPok.image = pokemonArray[0].image
+                }
+                
+            }
+            case .left:
+                print("gauche")
+                PokemonService.shared.fetchPokemon { (pokemonArray) in
+                DispatchQueue.main.sync{
+                    self.namePokLabel.text = pokemonArray[0].name
+                    self.typePokLabel.text = pokemonArray[0].type
+                    self.imgPok.image = pokemonArray[0].image
+                }
+                
+            }
+            default:
+                break
+            }
+        }
+    }
     
     
 
