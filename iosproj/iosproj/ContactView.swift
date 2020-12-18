cbd2bcdbd3a69dbc8a211bc721c87cd3a7897ee0
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2020-12-18 11:03:54 +0100

    Feat: correction

--- iosproj/iosproj/ContactView.swift
+++ iosproj/iosproj/ContactView.swift
@@ -5,25 +5,25 @@
 //  Created by Riyad Achour  on 17/12/2020.
 //
 
-import SwiftUI
-
-struct ContactView: View {
-    var body: some View {
-        VStack {
-            Text("Contactez nous")
-                .font(.title)
-                .foregroundColor(Color.white)
-            Text("poke@gmail.com")
-                .foregroundColor(Color.white)
-        }.background(
-            Image("Contactscreen-1")
-        )
-    }
-}
-
-struct ContactView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContactView()
-            
-    }
-}
+//import SwiftUI
+//
+//struct ContactView: View {
+//    var body: some View {
+//        VStack {
+//            Text("Contactez nous")
+//                .font(.title)
+//                .foregroundColor(Color.white)
+//            Text("poke@gmail.com")
+//                .foregroundColor(Color.white)
+//        }.background(
+//            Image("Contactscreen-1")
+//        )
+//    }
+//}
+//
+//struct ContactView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContactView()
+//
+//    }
+//}
